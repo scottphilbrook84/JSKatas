fa212c1ab65710f1ae5d07aa538957639028f56a
ernieballsep <ernieballsep@gmail.com>
2016-02-06 14:01:26 -0500

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var expect = require('chai').expect;
+var app = require('../lib/app.js')
+
+it('should equal bar', function() {
+    expect(app._test.foo()).to.equal('bar');
+});
+
+
+it('should equal infinity', function() {
+    expect(app._test.INFINITY).to.equal('infinitydd');
+});
+
